a4e2f94c2a4dae47c4659bf34ae448dbdec42f66
taga-dysong <116068642+butcreep@users.noreply.github.com>
2025-04-08 16:12:53 +0900

    chore: 초기 설정

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-gradient-to-r from-pink-500 to-yellow-500 text-white text-3xl">
+      초기 세팅 완료 🎉
+    </div>
+  );
+}
