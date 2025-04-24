525b9102d174e99767b56f18393d3706b02f63d6
Song da young <116068642+butcreep@users.noreply.github.com>
2025-04-24 23:44:16 +0900

    feat: 로그인 페이지 추가 (/login)

---
+++ src/app/login/page.tsx
@@ -0,0 +1,11 @@
+import { InstagramLoginButton } from '@/features/auth/instagram/ui/InstagramLoginButton';
+
+const LoginPage = () => {
+  return (
+    <div>
+      <InstagramLoginButton />
+    </div>
+  );
+};
+
+export default LoginPage;
