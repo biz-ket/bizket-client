0ba697c5a43243ba2d05b97d4b827d80ea0ed68b
Song da young <116068642+butcreep@users.noreply.github.com>
2025-04-27 23:44:40 +0900

    feat: RootLayout에 로그인 및 콜백 경로에서 헤더 숨김 로직 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,18 +3,23 @@
 import Header from '@/shared/ui/layout/Header';
 import './globals.css';
 import RootLayoutProvider from './provider/RootLayoutProvider';
+import { usePathname } from 'next/navigation';
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const pathname = usePathname();
+  // 헤더를 숨기고 싶은 경로들
+  const hideHeaderOn = ['/login', '/login/callback'];
+  const showHeader = !hideHeaderOn.includes(pathname);
   return (
     <html lang="ko">
       <body>
         <RootLayoutProvider>
           <div className="w-full">
-            <Header />
+            {showHeader && <Header />}
             {children}
           </div>
         </RootLayoutProvider>
