0e6a692cbd7f31c44d736ae8d4dbae978dae4e03
Song da young <116068642+butcreep@users.noreply.github.com>
2025-04-27 23:33:34 +0900

    feat: API 요청 프록시를 위한 rewrites 설정 추가

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/api/auth/:path*',
+        destination: `${process.env.NEXT_PUBLIC_BIZKET_API_BASE_URL}/auth/:path*`,
+      },
+      {
+        source: '/api/members/:path*',
+        destination: `${process.env.NEXT_PUBLIC_BIZKET_API_BASE_URL}/members/:path*`,
+      },
+      {
+        source: '/api/members/me',
+        destination: `${process.env.NEXT_PUBLIC_BIZKET_API_BASE_URL}/members/me`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
