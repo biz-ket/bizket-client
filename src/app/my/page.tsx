e223298755b88403e2b8e39adac81c36d58b1acc
Song da young <116068642+butcreep@users.noreply.github.com>
2025-05-03 16:16:04 +0900

    fix: 마이페이지 새로고침 메인으로 넘어가는 오류 수정
    - isLoading , hasHydrated 플래그 추가

--- src/app/my/page.tsx
+++ src/app/my/page.tsx
@@ -3,18 +3,28 @@ import { useRouter } from 'next/navigation';
 import Container from '@/shared/ui/layout/Container';
 import { useEffect } from 'react';
 import { useCurrentUser } from '@/features/auth/hooks/useCurrentUser';
+import { useAuthStore } from '@/features/auth/model/useAuthStore';
 
 const MyPage = () => {
   const router = useRouter();
-  const { data: user, isError } = useCurrentUser();
-
+  const { data: user, isError, isLoading } = useCurrentUser();
+  const hasHydrated = useAuthStore((s) => s._hasHydrated);
+  const token = useAuthStore((s) => s.token);
+  console.log('[MyPage]', { hasHydrated, token, isError, user, isLoading });
   useEffect(() => {
+    if (!hasHydrated || isLoading) return;
+
     if (isError || !user) {
       router.push('/');
     }
-  }, [isError, user, router]);
+  }, [hasHydrated, isLoading, isError, user, router]);
 
-  if (isError || !user) return null;
+  if (!hasHydrated || isLoading) {
+    return null;
+  }
+  if (isError || !user) {
+    return null;
+  }
   return (
     <Container>
       <h1>내 정보</h1>
--- src/features/auth/hooks/useCurrentUser.ts
+++ src/features/auth/hooks/useCurrentUser.ts
@@ -9,11 +9,12 @@ export interface CurrentUser {
 
 export const useCurrentUser = () => {
   const token = useAuthStore((s) => s.token);
+  const hasHydrated = useAuthStore((s) => s._hasHydrated);
 
   return useQuery<CurrentUser, Error>({
     queryKey: ['currentUser'],
     queryFn: () => authFetch('/api/mypage/me') as Promise<CurrentUser>,
-    enabled: Boolean(token), // 토큰이 있어야만 호출
+    enabled: Boolean(token) && hasHydrated, // 토큰이 있어야만 호출
     staleTime: 1000 * 60 * 5,
   });
 };
