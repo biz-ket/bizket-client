9d81093f012673168c7abccf2e8898258ad8486d
JeongTaekCho <cjt3591@gmail.com>
2025-05-11 18:24:36 +0900

    feat: 콘텐츠 생성이력 삭제

--- src/features/create-marketing/api/deleteMarketing.ts
+++ src/features/create-marketing/api/deleteMarketing.ts
@@ -1,6 +1,8 @@
 import { fetchApi } from '@/shared/utils/fetchApi';
 
-export const deleteMarketing = async (id: number) => {
+export const deleteMarketing = async (id: number | null) => {
+  if (!id) return;
+
   const result = await fetchApi(`/marketing/contents/${id}`, {
     method: 'DELETE',
   });
