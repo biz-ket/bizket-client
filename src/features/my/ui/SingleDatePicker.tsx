3e2d2613092646c3ec3c9d04e76af0913b33c793
Song da young <116068642+butcreep@users.noreply.github.com>
2025-05-11 18:05:36 +0900

    rafactor: arrow function 수정

--- src/features/my/ui/SingleDatePicker.tsx
+++ src/features/my/ui/SingleDatePicker.tsx
@@ -12,17 +12,14 @@ interface SingleDatePickerProps {
   control: Control<ProfileFormValues>;
   name: 'startDate';
 }
-export function formatDate(date: Date): string {
+export const formatDate = (date: Date): string => {
   const year = date.getFullYear();
   const month = String(date.getMonth() + 1).padStart(2, '0');
   const day = String(date.getDate()).padStart(2, '0');
   return `${year}.${month}.${day}`;
-}
+};
 
-export default function SingleDatePicker({
-  control,
-  name,
-}: SingleDatePickerProps) {
+export const SingleDatePicker = ({ control, name }: SingleDatePickerProps) => {
   const [open, setOpen] = useState(false);
   const ref = useRef<HTMLDivElement>(null);
 
@@ -74,4 +71,5 @@ export default function SingleDatePicker({
       }}
     />
   );
-}
+};
+export default SingleDatePicker;
