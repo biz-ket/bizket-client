46b6393ff928ad22864ed3708b66b982b2a54601
sykim0181 <soyeon364@naver.com>
2025-05-12 17:26:53 +0900

    fix: 마이페이지 프로필 카드 ui 수정
    - 텍스트 말줄임표 설정
    - 프로필 이미지 기본 이미지 설정

--- src/features/my/ui/UserProfile.tsx
+++ src/features/my/ui/UserProfile.tsx
@@ -1,4 +1,5 @@
 import { CurrentUser } from '@/features/auth/hooks/useCurrentUser';
+import { useMemberInfo } from '@/features/auth/hooks/useMemberInfo';
 import Flex from '@/shared/ui/layout/Flex';
 import Image from 'next/image';
 
@@ -7,22 +8,30 @@ interface UserProfileProps {
 }
 
 const UserProfile = ({ user }: UserProfileProps) => {
+  const { data: memberInfo } = useMemberInfo();
+
   return (
     <Flex
       justify="between"
       align="center"
       className="w-[360px] h-full rounded-20 bg-primary-50 shadow-[0px_2px_10px_0px_rgba(0,0,0,0.08)] px-35"
     >
-      <div className="text-white">
-        <span className="heading-sm">{user.nickname}</span>
+      <div className="text-white overflow-hidden">
+        <div className="heading-sm max-w-full overflow-hidden whitespace-nowrap text-ellipsis">
+          {user.nickname}
+        </div>
         <Flex direction="col" gap={8} className="body-md-regular mt-27">
-          <span>{user.placeEmail}</span>
-          <span>{user.placePhoneNumber}</span>
+          <div className="max-w-full overflow-hidden whitespace-nowrap text-ellipsis">
+            {user.placeEmail}
+          </div>
+          <div className="max-w-full overflow-hidden whitespace-nowrap text-ellipsis">
+            {user.placePhoneNumber}
+          </div>
         </Flex>
       </div>
-      <div className="w-[126px] h-[126px] rounded-[50%] overflow-hidden bg-white">
+      <div className="w-[126px] h-[126px] shrink-0 rounded-[50%] overflow-hidden bg-white">
         <Image
-          src={'images/shared/character.svg'}
+          src={memberInfo?.profileImageUrl || 'images/shared/profile.svg'}
           width={126}
           height={126}
           alt="사용자 프로필 이미지"
