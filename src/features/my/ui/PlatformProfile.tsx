9cc0e48568fec79fcc4100ca80f947371e3fc4e6
sykim0181 <soyeon364@naver.com>
2025-05-13 01:51:11 +0900

    fix: 마이페이지 사용자 정보 ui 수정
    - 비즈니스 보고서 페이지 이동 버튼 추가

--- src/features/my/ui/PlatformProfile.tsx
+++ src/features/my/ui/PlatformProfile.tsx
@@ -5,7 +5,7 @@ const PlatformProfile = () => {
   const { data } = useMemberInfo();
 
   return (
-    <div className="flex-1 h-full ml-42">
+    <div className="flex-1 h-full overflow-hidden pl-42 pt-37">
       {data && (
         <Flex direction="col" gap={58}>
           <div className="body-sm-regular text-font-20">{`플랫폼 정보 >`}</div>
